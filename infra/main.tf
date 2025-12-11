3b672f83fd3931582e1c41db908980d43cd71001
Naga <snagendradba77@gmail.com>
2025-12-12 02:46:33 +0530

    Main tf

--- infra/main.tf
+++ infra/main.tf
@@ -2,9 +2,9 @@ provider "kubernetes" {
   config_path = "~/.kube/config"
 }
 
-# -----------------------
+# -------------------------------
 # Namespaces
-# -----------------------
+# -------------------------------
 resource "kubernetes_namespace_v1" "app_ns" {
   metadata {
     name = "flask-app-ns"
@@ -17,25 +17,9 @@ resource "kubernetes_namespace_v1" "db_ns" {
   }
 }
 
-# -----------------------
-# PostgreSQL Secret
-# -----------------------
-resource "kubernetes_secret_v1" "postgres_secret" {
-  metadata {
-    name      = "postgres-secret"
-    namespace = kubernetes_namespace_v1.db_ns.metadata[0].name
-  }
-
-  data = {
-    POSTGRES_USER     = base64encode("postgres")
-    POSTGRES_PASSWORD = base64encode("password")
-    POSTGRES_DB       = base64encode("appdb")
-  }
-}
-
-# -----------------------
-# Optional ConfigMap for App
-# -----------------------
+# -------------------------------
+# ConfigMap for Flask App
+# -------------------------------
 resource "kubernetes_config_map_v1" "app_config" {
   metadata {
     name      = "app-config"
@@ -44,6 +28,23 @@ resource "kubernetes_config_map_v1" "app_config" {
 
   data = {
     APP_ENV = "dev"
+  }
+}
+
+# -------------------------------
+# Secret for PostgreSQL
+# -------------------------------
+resource "kubernetes_secret_v1" "postgres_secret" {
+  metadata {
+    name      = "postgres-secret"
+    namespace = kubernetes_namespace_v1.db_ns.metadata[0].name
+  }
+
+  type = "Opaque"
+
+  data = {
+    POSTGRES_USER     = base64encode("postgres")
+    POSTGRES_PASSWORD = base64encode("StrongPassword123")
   }
 }
 
