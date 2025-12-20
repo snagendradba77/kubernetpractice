b1bb1918f129ad8db0d3b0e357872e8a942623ae
Naga <snagendradba77@gmail.com>
2025-12-20 11:25:06 +0530

    Deploying table

---
+++ db/changelog/002-create-users.sql
@@ -0,0 +1,8 @@
+-- 002-create-users.sql
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
